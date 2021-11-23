f134c17b8e4d2ded7268652057db6bed3e5b01fe
mvaivre <m.vaivre@gmail.com>
2021-11-23 09:02:29 +0100

    Upgrade URL regex to include localhost

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -203,8 +203,8 @@ const ClientSettings = () => {
   const editSettings = (v: Partial<Settings>) => {
     const newSettings = { ...tempSettings, ...v }
 
-    // Check if we need to append the http:// protocol if an IP is used
-    if (v.nodeHost?.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}[:[0-9]*]?$/)) {
+    // Check if we need to append the http:// protocol if an IP or localhost is used
+    if (v.nodeHost?.match(/^(?:(?:[0-9]{1,3}\.){3}[0-9]{1,3}|localhost)(?::[0-9]*)?$/)) {
       newSettings.nodeHost = `http://${v.nodeHost}`
     }
 
