e7ebe0154fa4e44e4a78a96274474f4c88c4f42a
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-12-09 17:42:09 +0100

    Simplify tests by utilizing @testing-library

--- src/tests/pages/HomePage.test.tsx
+++ src/tests/pages/HomePage.test.tsx
@@ -1,10 +1,7 @@
-import { render, unmountComponentAtNode } from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-import { findElementByText } from '../utils'
 import HomePage from '../../pages/HomePage'
-
-let container: Element
 
 const mockedHistoryPush = jest.fn()
 jest.mock('react-router', () => ({
@@ -14,80 +11,63 @@ jest.mock('react-router', () => ({
   })
 }))
 
-beforeEach(() => {
-  container = document.createElement('div')
-  document.body.appendChild(container)
+it('welcomes the new user and displays initial actions', () => {
+  render(<HomePage hasWallet={false} usernames={[]} />)
+
+  const main = screen.getByRole('main')
+  expect(main).toHaveTextContent('Welcome!')
+  expect(main).not.toHaveTextContent('Welcome back!')
+  expect(main).toHaveTextContent('Please choose whether you want to create a new wallet or import an existing one.')
+  expect(screen.queryByRole('button', { name: 'New wallet' })).toBeInTheDocument()
+  expect(screen.queryByRole('button', { name: 'Import wallet' })).toBeInTheDocument()
+  expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 })
 
-afterEach(() => {
-  unmountComponentAtNode(container)
-  container.remove()
-  container = null
-})
+it('welcomes the user back and displays the login form', () => {
+  render(<HomePage hasWallet={true} usernames={['John Doe']} />)
 
-it('renders correct welcome message and actions', () => {
-  act(() => {
-    render(<HomePage hasWallet={true} usernames={['John Doe']} />, container)
-  })
-  expect(container.textContent).toContain('Welcome back!')
-  expect(container.textContent).toContain('Please choose an account and enter your password to continue')
-  expect(container.textContent).toContain('Account')
-  expect(container.textContent).toContain('Password')
-  expect(container.textContent).toContain('Login')
-  expect(container.textContent).toContain('Create / import a new wallet')
-  expect(container.textContent).not.toContain('New wallet')
-  expect(container.textContent).not.toContain('Import wallet')
-  expect(document.querySelector('[data-testid="home-sidebar"]')).toBeInTheDocument()
-
-  act(() => {
-    render(<HomePage hasWallet={false} usernames={[]} />, container)
-  })
-  expect(container.textContent).toContain('Welcome!')
-  expect(container.textContent).not.toContain('Welcome back!')
-  expect(container.textContent).toContain(
-    'Please choose whether you want to create a new wallet or import an existing one.'
-  )
-  expect(container.textContent).toContain('New wallet')
-  expect(container.textContent).toContain('Import wallet')
-  expect(document.querySelector('[data-testid="home-sidebar"]')).toBeInTheDocument()
+  const main = screen.getByRole('main')
+  expect(main).toHaveTextContent('Welcome back!')
+  expect(main).toHaveTextContent('Please choose an account and enter your password to continue')
+  expect(screen.getByLabelText('Account')).toBeInTheDocument()
+  expect(screen.getByLabelText('Password')).toBeInTheDocument()
+  expect(main).toHaveTextContent('Create / import a new wallet')
+  expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument()
+  expect(screen.queryByRole('button', { name: 'New wallet' })).not.toBeInTheDocument()
+  expect(screen.queryByRole('button', { name: 'Import wallet' })).not.toBeInTheDocument()
+  expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 })
 
 it('navigates correctly between "New account" and login pages', () => {
-  act(() => {
-    render(<HomePage hasWallet={true} usernames={['John Doe']} />, container)
-  })
-  expect(container.textContent).toContain('Create / import a new wallet')
-  expect(document.querySelector('[data-testid="home-sidebar"]')).toBeInTheDocument()
+  render(<HomePage hasWallet={true} usernames={['John Doe']} />)
 
-  let link = findElementByText('p', 'Create / import a new wallet')
-  link.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-  expect(container.textContent).toContain('New account')
-  expect(container.textContent).toContain(
-    'Please choose whether you want to create a new wallet or import an existing one.'
-  )
+  const main = screen.getByRole('main')
 
-  link = findElementByText('p', 'Use an existing account')
-  link.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-  expect(container.textContent).toContain('Welcome back!')
-  expect(container.textContent).toContain('Please choose an account and enter your password to continue')
+  let link = screen.getByText('Create / import a new wallet')
+  fireEvent.click(link)
+  expect(main).toHaveTextContent('New account')
+  expect(main).toHaveTextContent('Please choose whether you want to create a new wallet or import an existing one.')
+
+  link = screen.getByText('Use an existing account')
+  fireEvent.click(link)
+  expect(main).toHaveTextContent('Welcome back!')
+  expect(main).toHaveTextContent('Please choose an account and enter your password to continue')
 })
 
 it('navigates to new wallet creation page', () => {
-  act(() => {
-    render(<HomePage hasWallet={false} usernames={[]} />, container)
-  })
-  const button = findElementByText('button', 'New wallet')
-  button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  render(<HomePage hasWallet={false} usernames={[]} />)
+
+  const button = screen.getByRole('button', { name: 'New wallet' })
+  fireEvent.click(button)
   expect(mockedHistoryPush).toHaveBeenCalledTimes(1)
   expect(mockedHistoryPush).toHaveBeenCalledWith('/create')
 })
 
 it('navigates to new wallet import page', () => {
-  act(() => {
-    render(<HomePage hasWallet={false} usernames={[]} />, container)
-  })
-  const button = findElementByText('button', 'Import wallet')
-  button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  render(<HomePage hasWallet={false} usernames={[]} />)
+
+  const button = screen.getByRole('button', { name: 'Import wallet' })
+  fireEvent.click(button)
   expect(mockedHistoryPush).toHaveBeenCalledTimes(1)
   expect(mockedHistoryPush).toHaveBeenCalledWith('/import')
 })
