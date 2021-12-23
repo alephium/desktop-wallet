e0aadd9875cc0923710ade3859e8cc3862992a3b
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-12-23 10:10:05 +0100

    Fix order of imports

--- src/pages/Wallet/AddressPage.tsx
+++ src/pages/Wallet/AddressPage.tsx
@@ -16,8 +16,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import QRCode from 'react-qr-code'
 import { useContext } from 'react'
+import QRCode from 'react-qr-code'
 import styled, { useTheme } from 'styled-components'
 
 import { GlobalContext } from '../../App'
--- src/tests/numbers.test.ts
+++ src/tests/numbers.test.ts
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { abbreviateAmount, removeTrailingZeros, calAmountDelta } from '../utils/numbers'
+import { abbreviateAmount, calAmountDelta, removeTrailingZeros } from '../utils/numbers'
 import transactions from './fixtures/transactions.json'
 
 const alf = (amount: bigint) => {
