58e67af191b6b217a3a7ecf1d10b4fcc6c8c28f0
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-01-06 16:28:30 +0100

    Add password check before sending transaction
    Closes #52

--- src/pages/Settings/SecretPhraseModal.tsx
+++ src/pages/Settings/SecretPhraseModal.tsx
@@ -16,61 +16,29 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { getStorage, Wallet, walletOpen } from 'alephium-js'
 import { Edit3 } from 'lucide-react'
-import { ChangeEvent, useState } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 
-import { Button } from '../../components/Buttons'
 import InfoBox from '../../components/InfoBox'
-import Input from '../../components/Inputs/Input'
 import Modal from '../../components/Modal'
 import { Section } from '../../components/PageComponents/PageContainers'
-import Paragraph from '../../components/Paragraph'
+import PasswordConfirmation from '../../components/PasswordConfirmation'
 import { useGlobalContext } from '../../contexts/global'
 
-const Storage = getStorage()
-
 const SecretPhraseModal = ({ onClose }: { onClose: () => void }) => {
-  const { currentUsername, setSnackbarMessage } = useGlobalContext()
-  const [typedPassword, setTypedPassword] = useState('')
+  const { wallet } = useGlobalContext()
   const [isDisplayingPhrase, setIsDisplayingPhrase] = useState(false)
-  const [decryptedWallet, setDecryptedWallet] = useState<Wallet>()
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTypedPassword(e.target.value)
-  }
-
-  const handlePasswordVerification = async (callback: () => void) => {
-    const walletEncrypted = Storage.load(currentUsername)
-
-    try {
-      const plainWallet = await walletOpen(typedPassword, walletEncrypted)
-
-      if (plainWallet) {
-        setDecryptedWallet(plainWallet)
-        callback()
-      }
-    } catch (e) {
-      setSnackbarMessage({ text: 'Invalid password', type: 'alert' })
-    }
-  }
 
   return (
     <Modal title="Secret phrase" onClose={onClose} focusMode>
       {!isDisplayingPhrase ? (
         <div>
-          <Section>
-            <Input value={typedPassword} placeholder="Password" type="password" onChange={handlePasswordChange} />
-            <Paragraph secondary centered>
-              Type your password above to show your 24 words phrase.
-            </Paragraph>
-          </Section>
-          <Section>
-            <Button onClick={() => handlePasswordVerification(() => setIsDisplayingPhrase(true))} submit>
-              Show
-            </Button>
-          </Section>
+          <PasswordConfirmation
+            text="Type your password above to show your 24 words phrase."
+            buttonText="Show"
+            onCorrectPasswordEntered={() => setIsDisplayingPhrase(true)}
+          />
         </div>
       ) : (
         <Section>
@@ -79,7 +47,7 @@ const SecretPhraseModal = ({ onClose }: { onClose: () => void }) => {
             Icon={Edit3}
             importance="alert"
           />
-          <PhraseBox>{decryptedWallet?.mnemonic || 'No mnemonic was stored along with this wallet'}</PhraseBox>
+          <PhraseBox>{wallet?.mnemonic || 'No mnemonic was stored along with this wallet'}</PhraseBox>
         </Section>
       )}
     </Modal>
