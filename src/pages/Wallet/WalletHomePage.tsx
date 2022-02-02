105bb08e3d85ff1ce8b285e5c53afb8a40d93593
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-02-02 11:06:13 +0100

    Remove useless async keyword

--- src/pages/Wallet/WalletHomePage.tsx
+++ src/pages/Wallet/WalletHomePage.tsx
@@ -146,7 +146,7 @@ const WalletHomePage = () => {
     [client, loadedTxList, setLoadedTxList, setSnackbarMessage, wallet]
   )
 
-  const refreshData = async () => {
+  const refreshData = () => {
     fetchBalanceAndLatestTransactions()
     refreshAddressesDetails()
   }
