8d86e42c33abddaffcec60c7b96b7ff782d442e2
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-02-18 15:37:33 +0100

    Add overview page header with summary card

--- src/pages/Wallet/OverviewPage.tsx
+++ src/pages/Wallet/OverviewPage.tsx
@@ -20,15 +20,22 @@ import { calAmountDelta } from 'alephium-js'
 import { Transaction } from 'alephium-js/api/explorer'
 import dayjs from 'dayjs'
 import { AnimatePresence } from 'framer-motion'
+import { Check, Lock } from 'lucide-react'
 import { FC, useState } from 'react'
+import styled from 'styled-components'
 
 import ActionLink from '../../components/ActionLink'
+import Amount from '../../components/Amount'
 import Badge from '../../components/Badge'
 import Label from '../../components/Label'
 import { MainContent } from '../../components/PageComponents/PageContainers'
 import { PageH2 } from '../../components/PageComponents/PageHeadings'
 import Table, { TableCell, TableCellPlaceholder, TableProps, TableRow } from '../../components/Table'
 import { Address, useAddressesContext } from '../../contexts/addresses'
+import { useGlobalContext } from '../../contexts/global'
+import DayskyImage from '../../images/daysky.jpeg'
+import NightskyImage from '../../images/nightsky.png'
+import { appHeaderHeight } from '../../style/globalStyles'
 import TransactionDetailsModal from './TransactionDetailsModal'
 
 const transactionsTableHeaders: TableProps['headers'] = [
@@ -45,6 +52,12 @@ type TransactionAndAddress = Transaction & { address: Address }
 const OverviewPage: FC = () => {
   const [selectedTransaction, setSelectedTransaction] = useState<TransactionAndAddress>()
   const { addresses, fetchAddressTransactionsNextPage } = useAddressesContext()
+  const { currentUsername } = useGlobalContext()
+
+  const totalBalance = addresses.reduce((acc, address) => acc + BigInt(address.details.balance), BigInt(0))
+  const totalAvailableBalance = addresses.reduce((acc, address) => acc + address.availableBalance, BigInt(0))
+  const totalLockedBalance = addresses.reduce((acc, address) => acc + BigInt(address.details.lockedBalance), BigInt(0))
+  const totalNumberOfTransactions = addresses.map((address) => address.details.txNumber).reduce((a, b) => a + b, 0)
 
   const allConfirmedTxs = addresses
     .map((address) => address.transactions.confirmed.map((tx) => ({ ...tx, address })))
@@ -56,8 +69,6 @@ const OverviewPage: FC = () => {
     .flat()
     .sort((a, b) => b.timestamp - a.timestamp)
 
-  const totalNumberOfTransactions = addresses.map((address) => address.details.txNumber).reduce((a, b) => a + b, 0)
-
   const onTransactionClick = (transaction: TransactionAndAddress) => {
     setSelectedTransaction(transaction)
   }
@@ -66,8 +77,34 @@ const OverviewPage: FC = () => {
     addresses.forEach((address) => fetchAddressTransactionsNextPage(address))
   }
 
+  console.log(NightskyImage)
+
   return (
     <MainContent>
+      <Header>
+        <SummaryCard>
+          <AmountContainer>
+            <AmountStyled value={totalBalance} />
+            Total balance
+          </AmountContainer>
+          <Divider />
+          <Balances>
+            <Balance>
+              <Check size="12px" />
+              <span>
+                Available: <Amount value={totalAvailableBalance} />
+              </span>
+            </Balance>
+            <BalanceLocked>
+              <Lock size="12px" />
+              <span>
+                Locked: <Amount value={totalLockedBalance} />
+              </span>
+            </BalanceLocked>
+          </Balances>
+          <AccountName>{currentUsername}</AccountName>
+        </SummaryCard>
+      </Header>
       <PageH2>Transaction history</PageH2>
       <Table headers={transactionsTableHeaders} minColumnWidth={minTableColumnWidth}>
         {allPendingTxs
@@ -141,5 +178,69 @@ const OverviewPage: FC = () => {
     </MainContent>
   )
 }
+
+const Header = styled.header`
+  background-image: url(${({ theme }) => (theme.name === 'dark' ? NightskyImage : DayskyImage)});
+  background-position: bottom;
+  background-size: cover;
+  margin-top: calc((10px + ${appHeaderHeight}) * -1);
+  margin-left: -56px;
+  margin-right: -56px;
+  padding: 56px;
+  border-bottom: 1px solid ${({ theme }) => theme.border.secondary};
+`
+
+const SummaryCard = styled.div`
+  border-radius: 9px;
+  border: 1px solid ${({ theme }) => theme.border.secondary};
+  background-color: ${({ theme }) => theme.bg.primary};
+  padding: var(--spacing-4);
+  width: 300px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.7);
+`
+
+const AmountStyled = styled(Amount)`
+  font-size: 32px;
+  font-weight: var(--fontWeight-medium);
+  display: block;
+  color: ${({ theme }) => theme.font.primary};
+  margin-bottom: 3px;
+`
+
+const AmountContainer = styled.div`
+  color: ${({ theme }) => theme.font.secondary};
+`
+
+const Divider = styled.div`
+  width: 24px;
+  height: 1px;
+  background-color: ${({ theme }) => theme.border.primary};
+  margin: var(--spacing-3) 0;
+`
+
+const Balance = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: var(--fontWeight-medium);
+  gap: var(--spacing-1);
+`
+
+const Balances = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-2);
+`
+
+const BalanceLocked = styled(Balance)`
+  color: ${({ theme }) => theme.font.secondary};
+`
+
+const AccountName = styled.div`
+  color: ${({ theme }) => theme.font.secondary};
+  font-size: 10px;
+  margin-bottom: -8px;
+  text-transform: uppercase;
+  text-align: right;
+`
 
 export default OverviewPage
