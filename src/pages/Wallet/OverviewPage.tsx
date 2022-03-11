d4cee1d613a6015a53656f6348986c8ca8ce3163
mvaivre <m.vaivre@gmail.com>
2022-03-11 12:34:20 +0100

    Tweak AddressSummaryCards

--- src/pages/Wallet/OverviewPage.tsx
+++ src/pages/Wallet/OverviewPage.tsx
@@ -28,7 +28,6 @@ import AccountSummaryCard from '../../components/AccountSummaryCard'
 import ActionLink from '../../components/ActionLink'
 import AddressBadge from '../../components/AddressBadge'
 import AddressSummaryCard, { addressSummaryCardWidthPx } from '../../components/AddressSummaryCard'
-import Badge from '../../components/Badge'
 import Button from '../../components/Button'
 import GradientCanvas from '../../components/GradientCanvas'
 import { MainContent } from '../../components/PageComponents/PageContainers'
@@ -197,8 +196,7 @@ export default OverviewPage
 
 const addressSummaryCardsGapPx = 15
 const expandButtonLeftMarginPx = 20
-const collapsedaddressSummaryCardWidthPx = 8
-const scrollableCardsSectionPaddingPx = 71
+const collapsedaddressSummaryCardWidthPx = 10
 
 const Header = styled.header`
   position: relative;
@@ -214,7 +212,6 @@ const Header = styled.header`
 
 const Summaries = styled.div`
   display: flex;
-  align-items: center;
 `
 
 const AddressSummaryCards = styled.div<{ collapsed: boolean; totalAddresses: number }>`
@@ -224,13 +221,9 @@ const AddressSummaryCards = styled.div<{ collapsed: boolean; totalAddresses: num
   margin-left: calc(var(--spacing-2) * -1);
   padding-left: var(--spacing-4);
   align-items: center;
-  padding-bottom: ${scrollableCardsSectionPaddingPx}px;
-  margin-bottom: -${scrollableCardsSectionPaddingPx}px;
-  padding-top: ${scrollableCardsSectionPaddingPx}px;
-  margin-top: -${scrollableCardsSectionPaddingPx}px;
   width: ${({ collapsed, totalAddresses }) =>
     collapsed
-      ? `${totalAddresses * collapsedaddressSummaryCardWidthPx}px`
+      ? `${totalAddresses * collapsedaddressSummaryCardWidthPx + addressSummaryCardsGapPx}px`
       : `${
           totalAddresses * (addressSummaryCardWidthPx + addressSummaryCardsGapPx) +
           (expandButtonLeftMarginPx - addressSummaryCardsGapPx)
@@ -254,4 +247,5 @@ const ExpandButton = styled(Button)`
   margin-left: ${expandButtonLeftMarginPx}px;
   gap: var(--spacing-1);
   z-index: 1;
+  align-self: center;
 `
