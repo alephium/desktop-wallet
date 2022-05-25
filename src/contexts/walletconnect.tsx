polarker/alephium-wallet

Initial walletconnect support

src/contexts/walletconnect.tsx
@@ -18,18 +18,25 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 
 import WalletConnectClient, { CLIENT_EVENTS } from '@walletconnect/client'
 import { SessionTypes } from '@walletconnect/types'
+import { SignContractCreationTxParams, SignScriptTxParams, SignTransferTxParams } from 'alephium-web3'
 import { createContext, Dispatch, FC, SetStateAction, useContext, useEffect, useState } from 'react'
 
 import { useAddressesContext } from '../contexts/addresses'
-import { SendTransactionData } from '../modals/SendModal'
-import { useGlobalContext } from './global'
+import { BuildDeployContractTxData } from '../modals/SendModal/BuildDeployContractTx'
+import { BuildScriptTxData } from '../modals/SendModal/BuildScriptTx'
+import { BuildTransferTxData } from '../modals/SendModal/BuildTransferTx'
+import { TxModalType, useGlobalContext } from './global'
 
 export interface ContextType {
   isWalletConnectModalOpen: boolean
   setIsWalletConnectModalOpen: (isOpen: boolean) => void
   walletConnect?: WalletConnectClient
   setWalletConnect: Dispatch<SetStateAction<WalletConnectClient | undefined>>
-  dappTransactionData?: SendTransactionData
+  dappTransactionData?:
+    | ['transfer', BuildTransferTxData]
+    | ['deploy-contract', BuildDeployContractTxData]
+    | ['script', BuildScriptTxData]
+    | undefined
   requestEvent?: SessionTypes.RequestEvent
 }
 
@@ -45,12 +52,17 @@ export const initialContext: ContextType = {
 export const Context = createContext<ContextType>(initialContext)
 
 export const WalletConnectContextProvider: FC = ({ children }) => {
-  const { setIsSendModalOpen, settings } = useGlobalContext()
+  const { setTxModalType, settings } = useGlobalContext()
   const { addresses } = useAddressesContext()
   const [isWalletConnectModalOpen, setIsWalletConnectModalOpen] = useState(false)
   const [walletConnect, setWalletConnect] = useState<WalletConnectClient>()
-  const [dappTransactionData, setDappTransactionData] = useState<SendTransactionData>()
+  const [dappTransactionData, setDappTransactionData] = useState<ContextType['dappTransactionData']>()
   const [requestEvent, setRequestEvent] = useState<SessionTypes.RequestEvent>()
+
+  const setTxData = (type: TxModalType, data: typeof dappTransactionData[1]) => {
+    setDappTransactionData([type, data])
+    setTxModalType(type)
+  }
 
   useEffect(() => {
     if (walletConnect === undefined) {
@@ -77,53 +89,80 @@ export const WalletConnectContextProvider: FC = ({ children }) => {
       return
     }
 
+    const extractAddress = (signerAddress: string) => {
+      const address = addresses.find((a) => a.hash === signerAddress)
+      if (typeof address === 'undefined') {
+        throw new Error(`Unknown signer address: ${signerAddress}`)
+      }
+      return address
+    }
+
     const onSessionRequest = async (event: SessionTypes.RequestEvent) => {
       const {
         topic,
-        request: { id, method, params }
+        request: { method, params }
       } = event
       setRequestEvent(event)
 
-      if (method === 'alephium_getServices') {
-        await walletConnect.respond({
-          topic,
-          response: {
-            id,
-            jsonrpc: '2.0',
-            result: settings.network
+      try {
+        console.log(`============= ${method} ${JSON.stringify(params)}`)
+        if (method === 'alph_signTransferTx') {
+          const p = params as SignTransferTxParams
+          const txData: BuildTransferTxData = {
+            fromAddress: extractAddress(p.signerAddress),
+            toAddress: p.destinations[0].address,
+            alphAmount: p.destinations[0].alphAmount,
+            gasAmount: p.gasAmount,
+            gasPrice: p.gasPrice
+          }
+          setTxData('transfer', txData)
+        } else if (method === 'alph_signContractCreationTx') {
+          const p = params as SignContractCreationTxParams
+          const txData: BuildDeployContractTxData = {
+            fromAddress: extractAddress(p.signerAddress),
+            bytecode: p.bytecode,
+            initialFields: p.initialFields,
+            alphAmount: p.alphAmount,
+            issueTokenAmount: p.issueTokenAmount,
+            gasAmount: p.gasAmount,
+            gasPrice: p.gasPrice
+          }
+          console.log(
+            `========== txData : ${JSON.stringify(dappTransactionData, (key, value) =>
+              typeof value === 'bigint' ? value.toString() : value
+            )}`
+          )
+          setTxData('deploy-contract', txData)
+        } else if (method === 'alph_signScriptTx') {
+          const p = params as SignScriptTxParams
+          const txData: BuildScriptTxData = {
+            fromAddress: extractAddress(p.signerAddress),
+            bytecode: p.bytecode,
+            alphAmount: p.alphAmount,
+            gasAmount: p.gasAmount,
+            gasPrice: p.gasPrice
+          }
+          setTxData('script', txData)
+        } else {
+          throw new Error(`Unsupported walletconnect request: ${method}`)
+        }
+      } catch (e) {
+        console.warn(e)
+        let error: string
+        if (typeof e === 'string') {
+          error = e
+        } else if (e instanceof Error) {
+          error = e.message
+        } else {
+          error = 'Unknown internal error'
+        }
+        walletConnect.disconnect({
+          topic: topic,
+          reason: {
+            code: -32000,
+            message: error
           }
         })
-      } else if (method === 'alephium_signAndSubmitTx') {
-        const fromAddress = addresses.find((a) => a.hash === params.fromAddress)
-
-        if (fromAddress === undefined) {
-          await walletConnect.respond({
-            topic,
-            response: {
-              id,
-              jsonrpc: '2.0',
-              error: {
-                code: -32000,
-                message: 'Wallet is locked or invalid address specified.'
-              }
-            }
-          })
-        } else {
-          const txData = {
-            fromAddress,
-            toAddress: '',
-            amount: '',
-            gasAmount: params.gas,
-            script: params.script,
-            contractCode: params?.contract?.trim(),
-            contractState: params.state && JSON.stringify(params.state),
-            issueTokenAmount: params.issueTokenAmount
-          }
-          setDappTransactionData(txData)
-          setIsSendModalOpen(true)
-        }
-      } else {
-        console.warn('Unknown request given')
       }
     }
 
@@ -131,7 +170,7 @@ export const WalletConnectContextProvider: FC = ({ children }) => {
     return () => {
       walletConnect.removeListener(CLIENT_EVENTS.session.request, onSessionRequest)
     }
-  }, [walletConnect, addresses, setIsSendModalOpen, settings])
+  }, [walletConnect, addresses, setTxModalType, settings])
 
   return (
     <Context.Provider

src/modals/SendModal/BuildTransferTx.tsx
@@ -1,0 +1,77 @@
+/*
+Copyright 2018 - 2022 The Alephium Authors
+This file is part of the alephium project.
+
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { convertAlphToSet } from '@alephium/sdk'
+
+import { Address } from '../../contexts/addresses'
+import { isAmountWithinRange } from '../../utils/transactions'
+import { ModalContent, PartialTxData, SubmitOrCancel, ToAddress, useAddress, useBuildTxCommon } from './utils'
+
+export interface BuildTransferTxData {
+  fromAddress: Address
+  toAddress: string
+  alphAmount: string
+  gasAmount?: number
+  gasPrice?: string
+}
+
+export interface BuildTransferTxProps {
+  data: PartialTxData<BuildTransferTxData, 'fromAddress'>
+  onSubmit: (data: BuildTransferTxData) => void
+  onCancel: () => void
+}
+
+const BuildTransferTx = ({ data, onSubmit, onCancel }: BuildTransferTxProps) => {
+  const [fromAddress, FromAddress, alphAmount, AlphAmount, gasAmount, gasPrice, GasSettings, isCommonReady] =
+    useBuildTxCommon(data.fromAddress, data.alphAmount, data.gasAmount, data.gasPrice)
+  const [toAddress, handleAddressChange] = useAddress(data?.toAddress ?? '')
+
+  const isSubmitButtonActive =
+    isCommonReady &&
+    toAddress.value &&
+    !toAddress.error &&
+    alphAmount &&
+    isAmountWithinRange(convertAlphToSet(alphAmount), fromAddress.availableBalance)
+
+  console.log(`========= convert ${gasPrice.value} ${convertAlphToSet(gasPrice.value)}`)
+  return (
+    <>
+      <ModalContent>
+        {FromAddress}
+        <ToAddress toAddress={toAddress} handleAddressChange={handleAddressChange} />
+        {AlphAmount}
+      </ModalContent>
+      {GasSettings}
+      <SubmitOrCancel
+        onSubmit={() =>
+          onSubmit({
+            fromAddress: fromAddress,
+            toAddress: toAddress.value,
+            alphAmount: alphAmount,
+            gasAmount: gasAmount.value,
+            gasPrice: gasPrice.value ? gasPrice.value : undefined
+          })
+        }
+        onCancel={onCancel}
+        isSubmitButtonActive={isSubmitButtonActive}
+      />
+    </>
+  )
+}
+
+export default BuildTransferTx

src/utils/settings.ts
@@ -28,6 +28,7 @@ export interface Settings {
     passwordRequirement: boolean
   }
   network: {
+    networkId: number
     nodeHost: string
     explorerApiHost: string
     explorerUrl: string
@@ -41,13 +42,16 @@ export type UpdateSettingsFunctionSignature = <T extends keyof Settings>(
 
 type DeprecatedNetworkSettings = Settings['network']
 
-export const networkEndpoints: Record<Exclude<NetworkType, 'custom'>, Settings['network']> = {
+type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+export const networkEndpoints: Record<Exclude<NetworkType, 'custom'>, PartialBy<Settings['network'], 'networkId'>> = {
   mainnet: {
+    networkId: 0,
     nodeHost: 'https://mainnet-wallet.alephium.org',
     explorerApiHost: 'https://mainnet-backend.alephium.org',
     explorerUrl: 'https://explorer.alephium.org'
   },
   testnet: {
+    networkId: 1,
     nodeHost: 'https://testnet-wallet.alephium.org',
     explorerApiHost: 'https://testnet-backend.alephium.org',
     explorerUrl: 'https://testnet.alephium.org'
@@ -66,7 +70,7 @@ export const defaultSettings: Settings = {
     discreetMode: false,
     passwordRequirement: false
   },
-  network: clone(networkEndpoints.mainnet)
+  network: clone(networkEndpoints.mainnet) as Settings['network']
 }
 
 export const networkTypes = ['testnet', 'mainnet', 'localhost', 'custom'] as const

alephium/desktop-wallet

src/contexts/walletconnect.tsx
@@ -18,10 +18,14 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 
 import WalletConnectClient, { CLIENT_EVENTS } from '@walletconnect/client'
 import { SessionTypes } from '@walletconnect/types'
-import { createContext, Dispatch, FC, SetStateAction, useContext, useEffect, useState } from 'react'
+import { SignDeployContractTxParams, SignExecuteScriptTxParams, SignTransferTxParams } from 'alephium-web3'
+import { createContext, Dispatch, FC, SetStateAction, useCallback, useContext, useEffect, useState } from 'react'
 
 import { useAddressesContext } from '../contexts/addresses'
-import { SendTransactionData } from '../modals/SendModal'
+import { BuildDeployContractTxData } from '../modals/SendModal/BuildDeployContractTx'
+import { BuildScriptTxData } from '../modals/SendModal/BuildScriptTx'
+import { BuildTransferTxData } from '../modals/SendModal/BuildTransferTx'
+import { extractErrorMsg } from '../utils/misc'
 import { useGlobalContext } from './global'
 
 export interface ContextType {
@@ -29,8 +33,13 @@ export interface ContextType {
   setIsWalletConnectModalOpen: (isOpen: boolean) => void
   walletConnect?: WalletConnectClient
   setWalletConnect: Dispatch<SetStateAction<WalletConnectClient | undefined>>
-  dappTransactionData?: SendTransactionData
+  dappTransactionData?:
+    | ['transfer', BuildTransferTxData]
+    | ['deploy-contract', BuildDeployContractTxData]
+    | ['script', BuildScriptTxData]
+    | undefined
   requestEvent?: SessionTypes.RequestEvent
+  onError: (error: string) => void
 }
 
 export const initialContext: ContextType = {
@@ -39,18 +48,50 @@ export const initialContext: ContextType = {
   walletConnect: undefined,
   setWalletConnect: () => undefined,
   dappTransactionData: undefined,
-  requestEvent: undefined
+  requestEvent: undefined,
+  onError: (error: string) => undefined
 }
 
 export const Context = createContext<ContextType>(initialContext)
 
+const respondError = (walletConnect: WalletConnectClient, requestEvent: SessionTypes.RequestEvent, error: string) => {
+  walletConnect.respond({
+    topic: requestEvent.topic,
+    response: {
+      id: requestEvent.request.id,
+      jsonrpc: '2.0',
+      error: {
+        code: -32000,
+        message: error
+      }
+    }
+  })
+}
+
 export const WalletConnectContextProvider: FC = ({ children }) => {
-  const { setIsSendModalOpen, settings } = useGlobalContext()
+  const { setTxModalType, settings } = useGlobalContext()
   const { addresses } = useAddressesContext()
   const [isWalletConnectModalOpen, setIsWalletConnectModalOpen] = useState(false)
   const [walletConnect, setWalletConnect] = useState<WalletConnectClient>()
-  const [dappTransactionData, setDappTransactionData] = useState<SendTransactionData>()
+  const [dappTransactionData, setDappTransactionData] = useState<ContextType['dappTransactionData']>()
   const [requestEvent, setRequestEvent] = useState<SessionTypes.RequestEvent>()
+
+  const setTxData = useCallback(
+    (data: Exclude<ContextType['dappTransactionData'], undefined>) => {
+      setDappTransactionData(data)
+      setTxModalType(data[0])
+    },
+    [setDappTransactionData, setTxModalType]
+  )
+
+  const onError = useCallback(
+    (error: string): void => {
+      if (walletConnect && requestEvent) {
+        respondError(walletConnect, requestEvent, error)
+      }
+    },
+    [walletConnect, requestEvent]
+  )
 
   useEffect(() => {
     if (walletConnect === undefined) {
@@ -77,53 +118,59 @@ export const WalletConnectContextProvider: FC = ({ children }) => {
       return
     }
 
+    const extractAddress = (signerAddress: string) => {
+      const address = addresses.find((a) => a.hash === signerAddress)
+      if (typeof address === 'undefined') {
+        throw new Error(`Unknown signer address: ${signerAddress}`)
+      }
+      return address
+    }
+
     const onSessionRequest = async (event: SessionTypes.RequestEvent) => {
       const {
-        topic,
-        request: { id, method, params }
+        request: { method, params }
       } = event
       setRequestEvent(event)
 
-      if (method === 'alephium_getServices') {
-        await walletConnect.respond({
-          topic,
-          response: {
-            id,
-            jsonrpc: '2.0',
-            result: settings.network
+      try {
+        if (method === 'alph_signTransferTx') {
+          const p = params as SignTransferTxParams
+          const txData: BuildTransferTxData = {
+            fromAddress: extractAddress(p.signerAddress),
+            toAddress: p.destinations[0].address,
+            alphAmount: p.destinations[0].alphAmount,
+            gasAmount: p.gasAmount,
+            gasPrice: p.gasPrice
           }
-        })
-      } else if (method === 'alephium_signAndSubmitTx') {
-        const fromAddress = addresses.find((a) => a.hash === params.fromAddress)
-
-        if (fromAddress === undefined) {
-          await walletConnect.respond({
-            topic,
-            response: {
-              id,
-              jsonrpc: '2.0',
-              error: {
-                code: -32000,
-                message: 'Wallet is locked or invalid address specified.'
-              }
-            }
-          })
+          setTxData(['transfer', txData])
+        } else if (method === 'alph_signContractCreationTx') {
+          const p = params as SignDeployContractTxParams
+          const txData: BuildDeployContractTxData = {
+            fromAddress: extractAddress(p.signerAddress),
+            bytecode: p.bytecode,
+            initialAlphAmount: p.initialAlphAmount,
+            issueTokenAmount: p.issueTokenAmount,
+            gasAmount: p.gasAmount,
+            gasPrice: p.gasPrice
+          }
+          setTxData(['deploy-contract', txData])
+        } else if (method === 'alph_signScriptTx') {
+          const p = params as SignExecuteScriptTxParams
+          const txData: BuildScriptTxData = {
+            fromAddress: extractAddress(p.signerAddress),
+            bytecode: p.bytecode,
+            alphAmount: p.alphAmount,
+            gasAmount: p.gasAmount,
+            gasPrice: p.gasPrice
+          }
+          setTxData(['script', txData])
         } else {
-          const txData = {
-            fromAddress,
-            toAddress: '',
-            amount: '',
-            gasAmount: params.gas,
-            script: params.script,
-            contractCode: params?.contract?.trim(),
-            contractState: params.state && JSON.stringify(params.state),
-            issueTokenAmount: params.issueTokenAmount
-          }
-          setDappTransactionData(txData)
-          setIsSendModalOpen(true)
+          throw new Error(`Unsupported walletconnect request: ${method}`)
         }
-      } else {
-        console.warn('Unknown request given')
+      } catch (e) {
+        console.warn(e)
+        const error = extractErrorMsg(e)
+        respondError(walletConnect, event, error)
       }
     }
 
@@ -131,7 +178,7 @@ export const WalletConnectContextProvider: FC = ({ children }) => {
     return () => {
       walletConnect.removeListener(CLIENT_EVENTS.session.request, onSessionRequest)
     }
-  }, [walletConnect, addresses, setIsSendModalOpen, settings])
+  }, [walletConnect, addresses, setTxModalType, settings, setTxData])
 
   return (
     <Context.Provider
@@ -141,7 +188,8 @@ export const WalletConnectContextProvider: FC = ({ children }) => {
         setIsWalletConnectModalOpen,
         walletConnect,
         setWalletConnect,
-        dappTransactionData
+        dappTransactionData,
+        onError
       }}
     >
       {children}

src/modals/SendModal/BuildTransferTx.tsx
@@ -1,0 +1,81 @@
+/*
+Copyright 2018 - 2022 The Alephium Authors
+This file is part of the alephium project.
+
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { convertAlphToSet } from '@alephium/sdk'
+
+import { Address } from '../../contexts/addresses'
+import { isAmountWithinRange } from '../../utils/transactions'
+import { ModalContent, PartialTxData, SubmitOrCancel, ToAddress, useAddress, useBuildTxCommon } from './utils'
+
+export interface BuildTransferTxData {
+  fromAddress: Address
+  toAddress: string
+  alphAmount: string
+  gasAmount?: number
+  gasPrice?: string
+}
+
+export interface BuildTransferTxProps {
+  data: PartialTxData<BuildTransferTxData, 'fromAddress'>
+  onSubmit: (data: BuildTransferTxData) => void
+  onCancel: () => void
+}
+
+const BuildTransferTx = ({ data, onSubmit, onCancel }: BuildTransferTxProps) => {
+  const [fromAddress, FromAddress, alphAmount, AlphAmount, gasAmount, gasPrice, GasSettings, isCommonReady] =
+    useBuildTxCommon(data.fromAddress, data.alphAmount, data.gasAmount, data.gasPrice)
+  const [toAddress, handleAddressChange] = useAddress(data?.toAddress ?? '')
+
+  if (typeof fromAddress === 'undefined') {
+    onCancel()
+    return <></>
+  }
+
+  const isSubmitButtonActive =
+    isCommonReady &&
+    toAddress.value &&
+    !toAddress.error &&
+    alphAmount &&
+    isAmountWithinRange(convertAlphToSet(alphAmount), fromAddress.availableBalance)
+
+  return (
+    <>
+      <ModalContent>
+        {FromAddress}
+        <ToAddress toAddress={toAddress} handleAddressChange={handleAddressChange} />
+        {AlphAmount}
+      </ModalContent>
+      {GasSettings}
+      <SubmitOrCancel
+        onSubmit={() =>
+          onSubmit({
+            fromAddress: fromAddress,
+            toAddress: toAddress.value,
+            alphAmount: alphAmount,
+            gasAmount: gasAmount.parsed,
+            gasPrice: gasPrice.parsed
+          })
+        }
+        onCancel={onCancel}
+        isSubmitButtonActive={isSubmitButtonActive}
+      />
+    </>
+  )
+}
+
+export default BuildTransferTx

src/utils/settings.ts
@@ -28,6 +28,7 @@ export interface Settings {
     passwordRequirement: boolean
   }
   network: {
+    networkId: number
     nodeHost: string
     explorerApiHost: string
     explorerUrl: string
@@ -41,13 +42,16 @@ export type UpdateSettingsFunctionSignature = <T extends keyof Settings>(
 
 type DeprecatedNetworkSettings = Settings['network']
 
-export const networkEndpoints: Record<Exclude<NetworkName, 'custom'>, Settings['network']> = {
+type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+export const networkEndpoints: Record<Exclude<NetworkName, 'custom'>, PartialBy<Settings['network'], 'networkId'>> = {
   mainnet: {
+    networkId: 0,
     nodeHost: 'https://mainnet-wallet.alephium.org',
     explorerApiHost: 'https://mainnet-backend.alephium.org',
     explorerUrl: 'https://explorer.alephium.org'
   },
   testnet: {
+    networkId: 1,
     nodeHost: 'https://testnet-wallet.alephium.org',
     explorerApiHost: 'https://testnet-backend.alephium.org',
     explorerUrl: 'https://testnet.alephium.org'
@@ -66,7 +70,7 @@ export const defaultSettings: Settings = {
     discreetMode: false,
     passwordRequirement: false
   },
-  network: clone(networkEndpoints.mainnet)
+  network: clone(networkEndpoints.mainnet) as Settings['network']
 }
 
 export const networkNames = ['testnet', 'mainnet', 'localhost', 'custom'] as const