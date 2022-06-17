433cf1ebfad39ddc72cf154f1a94d6cb995dc617
LeeAlephium <lee@alephium.org>
2022-06-17 12:01:04 -0400

    Continually check for a new version each subsequent hour

--- src/hooks/useLatestGitHubRelease.ts
+++ src/hooks/useLatestGitHubRelease.ts
@@ -16,9 +16,11 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 
+import { useGlobalContext } from '../contexts/global'
 import { AppData, KEY_APPDATA, toAppData } from '../utils/app-data'
+import { useTimeout } from '../utils/hooks'
 
 const currentVersion = process.env.REACT_APP_VERSION
 const semverRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)?$/
@@ -26,8 +28,10 @@ const semverRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)?$/
 const ONE_HOUR = 1000 * 60 * 60
 
 const useLatestGitHubRelease = () => {
+  const { setSnackbarMessage } = useGlobalContext()
+
   const [newLatestRelease, setNewLatestRelease] = useState('')
-  const { lastVersionCheckedAt }: AppData = JSON.parse(localStorage.getItem(KEY_APPDATA) ?? '{}', toAppData) ?? {}
+  const [timeUntilNextFetch, setTimeUntilNextFetch] = useState(0)
 
   const fetchLatestVersion = async () => {
     const response = await fetch('https://api.github.com/repos/alephium/desktop-wallet/releases/latest')
@@ -39,15 +43,30 @@ const useLatestGitHubRelease = () => {
     }
   }
 
-  useEffect(() => {
-    if (lastVersionCheckedAt !== undefined && Date.now() - lastVersionCheckedAt.getTime() < ONE_HOUR) {
+  const tryFetchLatestVersion = useCallback(async () => {
+    try {
+      await fetchLatestVersion()
+    } catch (e) {
+      console.error(e)
+      setSnackbarMessage({ text: "Couldn't fetch latest wallet version.", type: 'alert' })
+    }
+  }, [setSnackbarMessage])
+
+  useTimeout(async () => {
+    const appData: AppData = JSON.parse(localStorage.getItem(KEY_APPDATA) ?? '{}', toAppData) ?? {}
+    const { lastVersionCheckedAt } = appData
+    const timeSinceLastCheck = (lastVersionCheckedAt !== undefined && Date.now() - lastVersionCheckedAt.getTime()) || 0
+    const nextTimeUntilNextFetch = Math.max(0, ONE_HOUR - timeSinceLastCheck)
+
+    if (timeUntilNextFetch === 0 && nextTimeUntilNextFetch !== 0) {
+      setTimeUntilNextFetch(nextTimeUntilNextFetch)
       return
     }
 
-    localStorage.setItem(KEY_APPDATA, JSON.stringify({ lastVersionCheckedAt: new Date() }))
-
-    fetchLatestVersion()
-  })
+    await tryFetchLatestVersion()
+    localStorage.setItem(KEY_APPDATA, JSON.stringify({ ...appData, lastVersionCheckedAt: new Date() }))
+    setTimeUntilNextFetch(nextTimeUntilNextFetch)
+  }, timeUntilNextFetch)
 
   return newLatestRelease
 }
