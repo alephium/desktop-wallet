33d6065629a8bd72cfa85493c5d0c354eb314855
LeeAlephium <lee@alephium.org>
2022-07-13 13:35:23 -0400

    Fix linting errors

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 import { motion } from 'framer-motion'
-import { FC, useEffect, useCallback } from 'react'
+import { FC, useCallback, useEffect } from 'react'
 import styled from 'styled-components'
 
 interface PopupProps {
