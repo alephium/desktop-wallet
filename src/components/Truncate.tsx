bba11ebcad2caca35154e560b762b0d5eb78a260
LeeAlephium <lee@alephium.org>
2022-08-22 13:46:54 -0400

    Refactor Truncate

--- src/components/Truncate.tsx
+++ src/components/Truncate.tsx
@@ -16,12 +16,9 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { FC } from 'react'
 import styled from 'styled-components'
 
-const Truncate: FC<{ className?: string }> = ({ className, children }) => <div className={className}>{children}</div>
-
-export default styled(Truncate)`
+export default styled(({ children, ...props }) => <div {...props}>{children}</div>)`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
