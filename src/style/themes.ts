315136fc372a22cfd20c72f2fbb1fc42d8d5d689
LeeAlephium <lee@alephium.org>
2022-09-06 09:32:59 -0400

    Switch from tinycolor2 to colord

--- src/style/themes.ts
+++ src/style/themes.ts
@@ -19,6 +19,8 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 import { colord } from 'colord'
 import { DefaultTheme } from 'styled-components'
 
+export type ThemeType = 'light' | 'dark'
+
 export const lightTheme: DefaultTheme = {
   name: 'light',
   bg: {
