b8259df65a48c1753ca0fc71729f589d131adda0
LeeAlephium <lee@alephium.org>
2022-09-06 09:32:59 -0400

    Switch from tinycolor2 to colord

--- src/components/Inputs/ColoredLabelInput.tsx
+++ src/components/Inputs/ColoredLabelInput.tsx
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { colord } from 'colord'
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
