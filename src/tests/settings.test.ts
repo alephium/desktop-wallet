d58fc71e3e2b96da921a1b12cf6cd69d80fc6a5e
mvaivre <m.vaivre@gmail.com>
2022-09-20 15:42:02 +0200

    Fix tests

--- src/tests/settings.test.ts
+++ src/tests/settings.test.ts
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { ThemeType } from '../style/themes'
+import { Language, ThemeType } from '../types/settings'
 import {
   defaultSettings,
   deprecatedSettingsExist,
@@ -33,7 +33,8 @@ const mockSettings = {
     theme: 'light' as ThemeType,
     walletLockTimeInMinutes: 999,
     discreetMode: false,
-    passwordRequirement: false
+    passwordRequirement: false,
+    language: 'en-US' as Language
   },
   network: {
     nodeHost: 'https://node',
