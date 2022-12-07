f63fe595b242706b0eb99eb13baeb005def32ab7
mvaivre <m.vaivre@gmail.com>
2022-12-07 17:51:58 +0100

    Migrating to Vite.js: app is starting Issue with the js-sdk though, the non minimized ver is used...

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -16,4 +16,4 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/// <reference types="react-scripts" />
+/// <reference types="vite/client" />
