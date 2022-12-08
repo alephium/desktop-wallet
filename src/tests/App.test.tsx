dadb74ce0c06854c59b7b13cb899d1ed56499efa
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-12-08 14:28:37 +0100

    Fix i18n warning in tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -15,6 +15,8 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
+import '../i18n'
+
 import { act, fireEvent, screen, waitFor } from '@testing-library/react'
 import { HashRouter as Router } from 'react-router-dom'
 import { PartialDeep } from 'type-fest'
