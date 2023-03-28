6f38e584036fe14934625c5ecf7ec8f4c6b6ee6f
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-03-28 16:32:00 +0200

    Update valid color

--- src/style/themes.ts
+++ src/style/themes.ts
@@ -52,7 +52,7 @@ export const lightTheme: DefaultTheme = {
     accent: '#5981f3',
     complementary: '#FF5D51',
     alert: '#ed4a34',
-    valid: '#0ea670',
+    valid: '#18BB63',
     highlightGradient: 'linear-gradient(45deg, rgba(18,0,218,1) 0%, rgba(255,93,81,1) 100%)'
   }
 }
@@ -90,7 +90,7 @@ export const darkTheme: DefaultTheme = {
     accent: '#598BED',
     complementary: '#FF5D51',
     alert: '#ed4a34',
-    valid: '#45d5ab',
+    valid: '#18BB63',
     highlightGradient: 'linear-gradient(45deg, rgba(18,0,218,1) 0%, rgba(255,93,81,1) 100%)'
   }
 }
