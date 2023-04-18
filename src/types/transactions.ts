91accf4fd34b913fadcfa923e542e8f79261e308
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-04-18 11:04:58 +0200

    Upgrade js-sdk

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { TransactionDirection, TransactionInfoType } from '@alephium/sdk'
 import { SweepAddressTransaction } from '@alephium/sdk/api/alephium'
 import { Output, Token, Transaction } from '@alephium/sdk/api/explorer'
 
@@ -148,7 +149,3 @@ export type CsvExportTimerangeQueryParams = {
 export type CsvExportQueryParams = CsvExportTimerangeQueryParams & {
   addressHash: AddressHash
 }
-
-// TODO: Transfer to SDK
-export type TransactionDirection = 'in' | 'out' | 'swap'
-export type TransactionInfoType = TransactionDirection | 'move' | 'pending'
