278f3ea57e257d8ce4029a89b7153d246bbcaf7e
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-04-25 14:28:39 +0200

    Fix tab bg and height of coming soon placeholder

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { colord } from 'colord'
 import { ChevronRight } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
@@ -103,6 +104,7 @@ export const Tab = styled.div<{ isActive: boolean }>`
         `
       : css`
           color: ${theme.font.tertiary};
+          background-color: ${({ theme }) => colord(theme.bg.background2).alpha(0.7).toHex()};
         `}
 
   &:not(:first-child) {
