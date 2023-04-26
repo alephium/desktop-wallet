d99ffb8799854d07573f88b29c65eb5af646c9af
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-04-26 18:33:19 +0200

    Add sidebar in every page
    Closes #648

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -16,21 +16,17 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
 
 import { fadeInSlowly } from '@/animations'
 import AppHeader from '@/components/AppHeader'
 import { FloatingPanel } from '@/components/PageComponents/PageContainers'
 import PanelTitle from '@/components/PageComponents/PanelTitle'
-import SideBar from '@/components/PageComponents/SideBar'
-import Scrollbar from '@/components/Scrollbar'
 import { useAppSelector } from '@/hooks/redux'
-import { ReactComponent as AlephiumLogotype } from '@/images/logotype.svg'
 import NewWalletActions from '@/pages/HomePage/NewWalletActions'
 import UnlockPanel from '@/pages/HomePage/UnlockPanel'
+import LockedWalletLayout from '@/pages/LockedWalletLayout'
 
 const HomePage = () => {
   const { t } = useTranslation()
@@ -39,61 +35,30 @@ const HomePage = () => {
   const [showNewWalletActions, setShowNewWalletActions] = useState(false)
 
   return (
-    <HomeContainer {...fadeInSlowly}>
-      <SideBar animateEntry>
-        <Logo>
-          <AlephiumLogotypeStyled />
-        </Logo>
-      </SideBar>
-      <Scrollbar>
-        <MainContent>
-          <FloatingPanel verticalAlign="center" horizontalAlign="center" transparentBg borderless>
-            {showNewWalletActions ? (
-              <>
-                <PanelTitle useLayoutId={false} size="big">
-                  {t('New wallet')}
-                </PanelTitle>
-                <NewWalletActions onExistingWalletLinkClick={() => setShowNewWalletActions(false)} />
-              </>
-            ) : hasAtLeastOneWallet ? (
-              <UnlockPanel onNewWalletLinkClick={() => setShowNewWalletActions(true)} />
-            ) : (
-              <>
-                <PanelTitle useLayoutId={false} size="big">
-                  {t('Welcome.')}
-                </PanelTitle>
-                <NewWalletActions />
-              </>
-            )}
-          </FloatingPanel>
-        </MainContent>
-        <AppHeader />
-      </Scrollbar>
-    </HomeContainer>
+    <LockedWalletLayout {...fadeInSlowly} animateSideBar>
+      <FloatingPanel verticalAlign="center" horizontalAlign="center" transparentBg borderless>
+        {showNewWalletActions ? (
+          <>
+            <PanelTitle useLayoutId={false} size="big">
+              {t('New wallet')}
+            </PanelTitle>
+            <NewWalletActions onExistingWalletLinkClick={() => setShowNewWalletActions(false)} />
+          </>
+        ) : hasAtLeastOneWallet ? (
+          <UnlockPanel onNewWalletLinkClick={() => setShowNewWalletActions(true)} />
+        ) : (
+          <>
+            <PanelTitle useLayoutId={false} size="big">
+              {t('Welcome.')}
+            </PanelTitle>
+            <NewWalletActions />
+          </>
+        )}
+      </FloatingPanel>
+
+      <AppHeader />
+    </LockedWalletLayout>
   )
 }
 
 export default HomePage
-
-const HomeContainer = styled(motion.main)`
-  display: flex;
-  flex: 1;
-  height: 100%;
-  background-color: ${({ theme }) => theme.bg.background1};
-`
-
-const Logo = styled.div`
-  padding: 5px;
-`
-
-const AlephiumLogotypeStyled = styled(AlephiumLogotype)`
-  fill: ${({ theme }) => theme.font.primary};
-  color: ${({ theme }) => theme.font.primary};
-`
-
-const MainContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 100vh;
-`
