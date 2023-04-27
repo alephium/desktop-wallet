d5042d4d2fa9e3efe012387331a867a584402a82
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-04-27 16:51:41 +0200

    Move delete contact action inside edit modal

--- src/modals/ContactFormModal.tsx
+++ src/modals/ContactFormModal.tsx
@@ -18,7 +18,9 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 
 import { getHumanReadableError } from '@alephium/sdk'
 import { isEmpty } from 'lodash'
+import { UserMinus } from 'lucide-react'
 import { usePostHog } from 'posthog-js/react'
+import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
@@ -26,7 +28,14 @@ import { InputFieldsColumn } from '@/components/InputFieldsColumn'
 import Input from '@/components/Inputs/Input'
 import { useAppDispatch } from '@/hooks/redux'
 import CenteredModal, { ModalFooterButton, ModalFooterButtons } from '@/modals/CenteredModal'
-import { contactStorageFailed, contactStoredInPersistentStorage } from '@/storage/addresses/addressesActions'
+import ConfirmModal from '@/modals/ConfirmModal'
+import ModalPortal from '@/modals/ModalPortal'
+import {
+  contactDeletedFromPeristentStorage,
+  contactDeletionFailed,
+  contactStorageFailed,
+  contactStoredInPersistentStorage
+} from '@/storage/addresses/addressesActions'
 import ContactsStorage from '@/storage/addresses/contactsPersistentStorage'
 import { Contact, ContactFormData } from '@/types/contacts'
 import {
@@ -50,6 +59,8 @@ const ContactFormModal = ({ contact, onClose }: ContactFormModalProps) => {
   })
   const posthog = usePostHog()
 
+  const [isDeleteContactModalOpen, setIsDeleteContactModalOpen] = useState(false)
+
   const errors = formState.errors
   const isFormValid = isEmpty(errors)
 
@@ -67,58 +78,93 @@ const ContactFormModal = ({ contact, onClose }: ContactFormModalProps) => {
     }
   }
 
+  const deleteContact = () => {
+    if (!contact) return
+
+    try {
+      ContactsStorage.deleteContact(contact)
+      dispatch(contactDeletedFromPeristentStorage(contact.id))
+      posthog?.capture('Deleted contact')
+    } catch (e) {
+      dispatch(contactDeletionFailed(getHumanReadableError(e, t('Could not delete contact.'))))
+    } finally {
+      onClose()
+    }
+  }
+
   return (
-    <CenteredModal title={t(contact ? 'Edit contact' : 'New contact')} onClose={onClose}>
-      <InputFieldsColumn>
-        <Controller
-          name="name"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input
-              label={t('Name')}
-              value={value}
-              onChange={onChange}
-              onBlur={onBlur}
-              error={errors.name?.type === 'required' ? requiredErrorMessage : errors.name?.message}
-              isValid={!!value && !errors.name}
-            />
+    <>
+      <CenteredModal title={t(contact ? 'Edit contact' : 'New contact')} onClose={onClose}>
+        <InputFieldsColumn>
+          <Controller
+            name="name"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label={t('Name')}
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                error={errors.name?.type === 'required' ? requiredErrorMessage : errors.name?.message}
+                isValid={!!value && !errors.name}
+              />
+            )}
+            rules={{
+              required: true,
+              validate: (name) => isContactNameValid({ name, id: contact?.id })
+            }}
+            control={control}
+          />
+          <Controller
+            name="address"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label={t('Address')}
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                error={errors.address?.type === 'required' ? requiredErrorMessage : errors.address?.message}
+                isValid={!!value && !errors.address}
+              />
+            )}
+            rules={{
+              required: true,
+              validate: {
+                isAddressValid,
+                isContactAddressValid: (address) => isContactAddressValid({ address, id: contact?.id })
+              }
+            }}
+            control={control}
+          />
+        </InputFieldsColumn>
+        <ModalFooterButtons>
+          {contact && (
+            <ModalFooterButton role="secondary" variant="alert" onClick={() => setIsDeleteContactModalOpen(true)}>
+              {t('Delete')}
+            </ModalFooterButton>
           )}
-          rules={{
-            required: true,
-            validate: (name) => isContactNameValid({ name, id: contact?.id })
-          }}
-          control={control}
-        />
-        <Controller
-          name="address"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input
-              label={t('Address')}
-              value={value}
-              onChange={onChange}
-              onBlur={onBlur}
-              error={errors.address?.type === 'required' ? requiredErrorMessage : errors.address?.message}
-              isValid={!!value && !errors.address}
-            />
-          )}
-          rules={{
-            required: true,
-            validate: {
-              isAddressValid,
-              isContactAddressValid: (address) => isContactAddressValid({ address, id: contact?.id })
-            }
-          }}
-          control={control}
-        />
-      </InputFieldsColumn>
-      <ModalFooterButtons>
-        <ModalFooterButton role="secondary" onClick={onClose}>
-          {t('Cancel')}
-        </ModalFooterButton>
-        <ModalFooterButton onClick={handleSubmit(saveContact)} disabled={!isFormValid}>
-          {t('Save')}
-        </ModalFooterButton>
-      </ModalFooterButtons>
-    </CenteredModal>
+          <ModalFooterButton role="secondary" onClick={onClose}>
+            {t('Cancel')}
+          </ModalFooterButton>
+          <ModalFooterButton onClick={handleSubmit(saveContact)} disabled={!isFormValid}>
+            {t('Save')}
+          </ModalFooterButton>
+        </ModalFooterButtons>
+      </CenteredModal>
+      <ModalPortal>
+        {contact && isDeleteContactModalOpen && (
+          <ConfirmModal
+            onConfirm={deleteContact}
+            onClose={() => setIsDeleteContactModalOpen(false)}
+            Icon={UserMinus}
+            narrow
+          >
+            {t('Are you sure you want to remove "{{ contactName }}" from your contact list?', {
+              contactName: contact.name
+            })}
+          </ConfirmModal>
+        )}
+      </ModalPortal>
+    </>
   )
 }
 
