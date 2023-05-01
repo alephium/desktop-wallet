ef3f613f64f0e679ff7e154ef27cb96248cebe1a
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-05-01 18:38:06 +0200

    Fix Windows deep linking

--- public/electron.js
+++ public/electron.js
@@ -24,12 +24,17 @@ const contextMenu = require('electron-context-menu')
 const { autoUpdater } = require('electron-updater')
 
 // Handle deep linking for alephium://
+
+const ALEPHIUM = 'alephium'
+const ALEPHIUM_WALLET_CONNECT_DEEP_LINK_PREFIX = `${ALEPHIUM}://wc`
+const ALEPHIUM_WALLET_CONNECT_URI_PREFIX = '?uri='
+
 if (process.defaultApp) {
   if (process.argv.length >= 2) {
-    app.setAsDefaultProtocolClient('alephium', process.execPath, [path.resolve(process.argv[1])])
+    app.setAsDefaultProtocolClient(ALEPHIUM, process.execPath, [path.resolve(process.argv[1])])
   }
 } else {
-  app.setAsDefaultProtocolClient('alephium')
+  app.setAsDefaultProtocolClient(ALEPHIUM)
 }
 
 contextMenu()
@@ -201,6 +206,14 @@ if (!gotTheLock) {
       if (mainWindow.isMinimized()) mainWindow.restore()
       mainWindow.focus()
     }
+
+    const url = commandLine.pop().slice(0, -1)
+
+    if (url && url.startsWith(ALEPHIUM_WALLET_CONNECT_DEEP_LINK_PREFIX)) {
+      const uri = url.split(ALEPHIUM_WALLET_CONNECT_URI_PREFIX).at(1)
+
+      mainWindow.webContents.send('wc:connect', uri)
+    }
   })
 
   // This method will be called when Electron has finished
@@ -263,7 +276,9 @@ if (!gotTheLock) {
   })
 
   app.on('open-url', (_, url) => {
-    const uri = url.replace('alephium://wc?uri=', '')
-    mainWindow.webContents.send('wc:connect', uri)
+    if (url.startsWith(ALEPHIUM_WALLET_CONNECT_DEEP_LINK_PREFIX)) {
+      const uri = url.split(ALEPHIUM_WALLET_CONNECT_URI_PREFIX).at(1)
+      mainWindow.webContents.send('wc:connect', uri)
+    }
   })
 }
