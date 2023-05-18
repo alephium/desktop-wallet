8caca3a6053d52fe47c57af638a366efdbcacfe0
mvaivre <m.vaivre@gmail.com>
2023-05-18 11:49:16 +0200

    Adapt i18n script to sort alphabetically

--- scripts/syncTranslationFiles.js
+++ scripts/syncTranslationFiles.js
@@ -30,15 +30,19 @@ const sync = () => {
       const data = JSON.parse(fs.readFileSync(filepath, { encoding: 'utf-8' }))
       const newData = {}
 
-      Object.keys(data).forEach((key) => {
-        if (source[key]) newData[key] = data[key]
-      })
+      Object.keys(data)
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+        .forEach((key) => {
+          if (source[key]) newData[key] = data[key]
+        })
 
-      Object.keys(source).forEach((key) => {
-        if (!newData[key]) newData[key] = source[key]
-      })
+      Object.keys(source)
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+        .forEach((key) => {
+          if (!newData[key]) newData[key] = source[key]
+        })
 
-      fs.writeFileSync(filepath, JSON.stringify(newData))
+      fs.writeFileSync(filepath, JSON.stringify(newData, null, 2)) // Null and 2 arguments to format the JSON in the file
     } catch (e) {
       console.error('Error occurred while parsing file "' + filepath + '"')
     }
