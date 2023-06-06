0cef8d560c16fafd48113ad06ecf0f06f96bc5aa
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-06-06 11:30:11 +0200

    Sort assets based on verified, name, and id props

--- src/storage/addresses/addressesSelectors.ts
+++ src/storage/addresses/addressesSelectors.ts
@@ -16,9 +16,10 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Asset, TokenDisplayBalances } from '@alephium/sdk'
+import { Asset, TokenDisplayBalances, UnverifiedAsset, VerifiedAsset } from '@alephium/sdk'
 import { ALPH } from '@alephium/token-list'
 import { createSelector } from '@reduxjs/toolkit'
+import { partition } from 'lodash'
 
 import { addressesAdapter, contactsAdapter } from '@/storage/addresses/addressesAdapters'
 import { selectAllAssetsInfo } from '@/storage/assets/assetsSelectors'
@@ -107,7 +108,17 @@ export const makeSelectAddressesAssets = () =>
         }
       })
 
-      return [alphAsset, ...tokenAssets]
+      const [verifiedTokens, remainingTokens] = partition(tokenAssets, (token) => token.verified)
+      const [unverifiedTokens, unknownTokens] = partition(remainingTokens, (token) => token.verified === false)
+
+      return [
+        alphAsset,
+        ...(verifiedTokens as VerifiedAsset[]).sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase())),
+        ...(unverifiedTokens as UnverifiedAsset[]).sort((a, b) =>
+          a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+        ),
+        ...unknownTokens.sort((a, b) => a.id.toLowerCase().localeCompare(b.id.toLowerCase()))
+      ]
     }
   )
 
