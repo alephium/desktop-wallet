dd9c3816ffab3ad79f757251002a795c07b2a719
Thomas Droxler <thomas.droxler@gmail.com>
2023-07-10 11:08:14 +0200

    Simplify and update nix package

--- alephium-wallet.nix
+++ alephium-wallet.nix
@@ -1,48 +1,11 @@
 { lib, stdenv , fetchurl , appimageTools , makeWrapper , electron }:
 
-stdenv.mkDerivation rec {
-  pname = "alephium-wallet";
-  version = "0.11.1";
+appimageTools.wrapType1 rec {
+  name = "alephium-wallet";
+  version = "2.0.2";
 
   src = fetchurl {
-    url = "https://github.com/alephium/alephium-wallet/releases/download/v${version}/alephium-wallet-linux-${version}.AppImage";
-    sha256 = "067nqh748n02cspaa7c74qfigg1qdb4amhgaq6fwyb3j6z3m3b4j";
-    name = "${pname}-${version}.AppImage";
-  };
-
-  appimageContents = appimageTools.extractType2 {
-    name = "${pname}-${version}";
-    inherit src;
-  };
-
-  dontUnpack = true;
-  dontConfigure = true;
-  dontBuild = true;
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin $out/share/${pname} $out/share/applications
-
-    cp -a ${appimageContents}/{locales,resources} $out/share/${pname}
-    cp -a ${appimageContents}/alephium.desktop $out/share/applications/${pname}.desktop
-    cp -a ${appimageContents}/usr/share/icons $out/share
-
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=${pname}'
-
-    runHook postInstall
-  '';
-
-  postFixup = ''
-    makeWrapper ${electron}/bin/electron $out/bin/${pname} \
-      --add-flags $out/share/${pname}/resources/app.asar
-  '';
-
-  meta = with lib; {
-    description = "Alephium desktop wallet";
-    homepage = "https://github.com/alephium/alephium-wallet";
+    url = "https://github.com/alephium/desktop-wallet/releases/download/v${version}/Alephium-${version}.AppImage";
+    sha256 = "9853c2aafa3b608a4f2f623129accb499ba25a1201ecb82c7a4c773d6a51f65b";
   };
 }
